15b7d149c707694a0f7d949da93ec55b3e6cdfc2
GioGreco <grecogiorgio.97@gmail.com>
2022-11-23 18:29:49 +0100

    (extra commit 1)

--- src/components/LearnMoreComponent.vue
+++ src/components/LearnMoreComponent.vue
@@ -2,12 +2,11 @@
     <section id="learn-more">
         <div class="wrapper">
             <div class="learnMore-left">
-                <!-- <img class="pic-behind" src="/images/busy-architect-PYVKWM4-1024x872.jpg" alt=""> -->
-                <div class="green-layover">
-                    <img class="pic-behind" src="/images/busy-architect-PYVKWM4-1024x872.jpg" alt="">
-                    <!-- <img class="pic-front" src="/images/businesswoman-analysing-document-P8WSNMC-1024x820.jpg" alt=""> -->
+                <div class="wrapper">
+                    <img class="pic-behind" src="/images/busy-architect-PYVKWM4-1024x872.jpg" alt="" v-motion :initial="{x: -5}" :visible="{x: 5, transition:{repeat: Infinity, repeatType: 'mirror', type: 'spring', stiffness: 10, mass: 5}}">
+                    <div class="green-layover"></div>
+                    <img class="pic-front" src="/images/businesswoman-analysing-document-P8WSNMC-1024x820.jpg" alt="" v-motion :initial="{x: -10}" :visible="{x: 10, transition:{repeat: Infinity, repeatType: 'mirror', type: 'spring', stiffness: 10, mass: 5}}">
                 </div>
-                <!-- <img class="pic-front" src="/images/businesswoman-analysing-document-P8WSNMC-1024x820.jpg" alt=""> -->
             </div>
             <div class="learnMore-right">
                 <h3>learn more about our <span>missions</span></h3>
@@ -41,21 +40,35 @@
             .learnMore-left{
                 width: 60%;
 
-                .green-layover{
+                .wrapper{
                     width: 80%;
+                    height: max-content;
                     border-top-right-radius: 20px;
                     border-bottom-right-radius: 20px;
+                    position: relative;
                     .pic-behind{
                         width: 100%;
                         border-top-right-radius: 20px;
                         border-bottom-right-radius: 20px;
                     }
+                    .green-layover{
+                        position: absolute;
+                        width: 100%;
+                        height: 100%;
+                        border-top-right-radius: 20px;
+                        border-bottom-right-radius: 20px;
+                        background-color: rgba($palegreen-bg, 0.6);
+                    }
+
+                    .pic-front{
+                        position: absolute;
+                        width: 100%;
+                        height: 100%;
+                        top: 15%;
+                        left: 10%;
+                        border-radius: 20px;
+                    }
                 }
-                // .pic-behind{
-                //     width: 80%;
-                //     border-top-right-radius: 20px;
-                //     border-bottom-right-radius: 20px;
-                // }
             }
 
             .learnMore-right{
